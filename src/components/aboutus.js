1e9dae4fc1f5786f2fcf55a8b44fa341f84887d3
mobo-luwaduro <akiyademoboluwaduro@gmail.com>
2024-12-10 14:17:46 -0800

    belsoft landing page

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -22,32 +22,6 @@ const AboutUs = () => {
     />
   </div>
 </section> 
-
-{/* <section className="relative bg-[#fdf7ff] py-12 px-6 md:px-20 flex flex-col md:flex-row items-center gap-10">
-    <div className="relative w-full sm:w-[90%] md:w-[1572px] h-[auto] sm:h-[400px] md:h-[564px] opacity-100 overflow-hidden rounded-lg" style={{ top: "1120px", left: "80px" }}>
-      <Image
-        src="/frame 317.png"
-        alt="About Us"
-        width={1572}
-        height={564}
-        className="w-full h-full object-cover"
-      />
-    </div>
-  </section> */}
-
-
-{/* <section className="relative bg-[#fdf7ff] py-12 px-6 md:px-20 flex flex-col md:flex-row items-center gap-10  overflow-x-hidden">
-    <div className="relative w-full sm:w-[90%] md:w-[1572px] h-auto sm:h-[400px] md:h-[564px] opacity-100 overflow-hidden rounded-lg" style={{ top: "1120px", left: "80px" }}>
-      <Image
-        src="/frame 317.png"
-        alt="About Us"
-        width={1572}
-        height={564}
-        className="w-full h-full object-cover"
-      />
-    </div>
-  </section> */}
-
   </>
   );
 };
--- src/components/hero.js
+++ src/components/hero.js
@@ -4,56 +4,6 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-  //   <div>
-  //   <div className="fixed top-0 z-50 w-full">
-  //     <Image
-  //       src="/frame 7.png"
-  //       alt="Header"
-  //       width={1728}
-  //       height={125}
-  //       className="w-full h-auto object-cover border-[0.5px] border-[#75687e] opacity-100"
-  //     />
-  //   </div>
-  
-  //   {/* Hero Content Section */}
-  //   <div className="pt-32 px-6 md:px-12 max-w-full mx-auto">
-  //     {/* Right Section Images */}
-  //     <div className="relative mt-10 md:mt-0">
-  //       {/* Large Frame Image */}
-  //       <div className="absolute top-[50px] left-[20px] sm:left-[50px] w-[100%] sm:w-[822px] h-[auto] sm:h-[681px] overflow-hidden">
-  //         <Image
-  //           src="/frame 25.png"
-  //           alt="Event Preview"
-  //           width={822}
-  //           height={681}
-  //           className="w-full h-full object-cover"
-  //         />
-  //       </div>
-  
-  //       {/* Vector Image */}
-  //       <div className="absolute top-[103px] left-[50px] sm:left-[50px] w-[100%] sm:w-[997px] h-[auto] sm:h-[1125.5px] overflow-hidden">
-  //         <Image
-  //           src="/vector 1.svg"
-  //           alt="Decorative Vector"
-  //           width={997}
-  //           height={1125.5}
-  //           className="w-full h-full"
-  //         />
-  //       </div>
-  
-  //       {/* Group Image */}
-  //       <div className="absolute top-[237.65px] left-[50px] sm:left-[946.38px] w-[100%] sm:w-[700.03px] h-[auto] sm:h-[746.96px] bg-transparent border-[1px] border-[#dcb6ff] rounded-[20px] overflow-hidden">
-  //         <Image
-  //           src="/group 3.png"
-  //           alt="Event Gallery"
-  //           width={700.03}
-  //           height={746.96}
-  //           className="w-full h-full object-cover"
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
   <div>
   <div className="fixed top-0 z-50 w-full">
     <Image
